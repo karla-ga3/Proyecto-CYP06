f83b31c9e302a211c00e09e6be196c4bf768aa9e
karla-ga3 <karlagardunoalonso.28@gmail.com>
2022-11-30 18:11:31 -0600

    Segunda versión proyecto CYP

--- corrector.cpp
+++ corrector.cpp
@@ -9,14 +9,23 @@ void Diccionario()
 		char palabra;
 
 	fopen_s(&archivo, ".txt", "w");
-	if (archivo != Null)
+	if (archivo != NULL)
 	{
 		scanf("%[^\n]", &palabra);
 		fflush(stdin);
+		for (in i = 0; palabra[i] != '\0'; i++)
+		{
+			palabra[i] = tolower(palabra[i]);
+		}
+		printf("%s", palabra);
 	}
 	else
 	{
 		printf("\n No detecta archivo \n\n");
 	}
 	fclose(archivo);
+}
+int main()
+{
+	return 0;
 }
